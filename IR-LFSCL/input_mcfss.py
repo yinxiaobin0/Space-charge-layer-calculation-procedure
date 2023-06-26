9b6b0e4f6e60df2b6ad6fc6e15efc6a10b55d00d
yxb <yinxiaobin2022@163.com>
2023-06-26 09:07:51 +0800

    procedure upload yxb

---
+++ IR-LFSCL/input_mcfss.py
@@ -0,0 +1,20 @@
+"""
+mcfss obtained based on the coordination field model is used to replace the potential
+"""
+"""sulfide cathode"""
+#potential_list = [-0.241,-0.225,-0.205,-0.211,-0.299,-0.266,-0.219,-0.123,-0.26,-0.239,-0.193,-0.23,-0.242,-0.153,-0.321]
+"""oxide cathode"""
+#potential_list = [-0.551, -0.57, -0.487, -0.498, -0.678, -0.718, -0.775, -0.814, -0.4, -0.489, -0.438, -0.691]
+"""oxide coating"""
+potential_list = [2.249, 2.669, 2.375, 2.523, 3.273]
+"""test"""
+#potential_list = [-0.321]
+
+
+"""
+band obtained based on the DFT is used to replace the potential
+"""
+"""oxide cathode band"""
+#potential_list = [-0.764, -1.398, -1.777, -1.213, -0.075, -0.307, -0.385, -0.384, -1.793, -1.831, -1.141, -2.068]
+"""oxide coating band"""
+#potential_list = [2.065, 0.751, 1.626, 1.001, 1.795]
